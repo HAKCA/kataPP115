cebc150670386965996935bcf2f50c09492b51d3
HAKCA <mr.hakca@yandex.ru>
2024-04-23 18:52:19 +0300

    I did D:

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,17 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    public Connection getConnection() {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/users", "root", "mySQLhakcaPASSword1337");
+            return connection;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
